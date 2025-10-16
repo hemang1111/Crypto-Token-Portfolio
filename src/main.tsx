e420a38a3572be9738760d089c23f10780bdb8ac
Hemang Jani <hemangjani1111@gmail.com>
2025-10-16 21:35:54 +0530

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'rsuite/dist/rsuite.min.css';
-import App from './App'
+import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { WalletProvider } from "./Components/WalletProvider";
